95dfa38c897bc9a3a27cb19b161806d04451eddc
paul pichon <paul10_barca@hotmail.com>
2023-01-31 17:31:56 -0600

    Menu de la aplicacion

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -12,33 +12,18 @@ const preguntas = [
       message: '¿Qué desea hacer?',
       choices: [
         {
-            value: '1',
-            name: `${'1.'.green} Crear tarea`
+            value: 1,
+            name: `${'1.'.green} Buscar ciudad`
         },
         {
-            value: '2',
-            name: `${'2.'.green} Listar tareas`
+            value: 2,
+            name: `${'2.'.green} Ver historial`
         },
         {
-            value: '3',
-            name: `${'3.'.green} Listar tareas completadas`
-        },
-        {
-            value: '4',
-            name: `${'4.'.green} Listar tareas pendientes`
-        },
-        {
-            value: '5',
-            name: `${'5.'.green} Completar tareas(s)`
-        },
-        {
-            value: '6',
-            name: `${'6.'.green} Borrar tarea`
-        },
-        {
-            value: '0',
+            value: 0,
             name: `${'0.'.green} Salir`
-        }
+        },
+        
       ],
     },
   ];
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-import { leerInput } from "./helpers/inquirer.js"
+import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js"
 
 const main = async() => {
-    const texto = await leerInput('Hola ');
+    
+    //definimos opt como un string vacio
+    let opt;
 
-    console.log( texto );
+    do {
+        //imprimir el menu
+        //al tener promesas en mensajes.js
+        //podemos hacer uso de await
+        //lo que le estamos diciendo es que mostrarMenu() debe esperar hastaque tenga una resolucion
+        opt = await inquirerMenu();
+        
+
+        console.log({ opt });
+
+        //condicion para poner pausa antes de que ejecutar la siguiente instruccion
+        if( opt !== 0) await pausa();
+        
+
+    } while ( opt !== 0);//mientras opt sea diferente de 0 el menu se repetira indefinidamente
+
+    
 }
 
 
